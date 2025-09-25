27309c5ed876d26cb5b07340668e5e47f0c79f5a
Joao Pedro Pereira Gonçalves Silveira <joaopedroh101@gmail.com>
2025-09-25 17:52:35 -0300

    Add files via upload

---
+++ Arquivos Faculdade/Faculdade/qds_heranca/Heranca-Java/src/main/java/br/edu/gitflow/modelo/Cachorro.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Cachorro extends Animal {
+    private String raca;
+
+    public Cachorro(String nome, int idade, String raca) {
+        super(nome, idade);
+        this.raca = raca;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" +
+                "Raça = " + raca;
+    }
+
+    public void latir() {
+        System.out.println(getNome() + " late, au au!");
+    }    
+}
